5930d3e8e98f197eccacf37ed51a20a64149e32d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-10-28 17:43:58 +0100

    all tests green

--- tests/unit/TribeEventsTicketStockObjectTest.php
+++ tests/unit/TribeEventsTicketStockObjectTest.php
@@ -8,11 +8,6 @@
 		protected $mock_ticket;
 
 		protected function setUp() {
-			$this->mock_ticket = $this->getMockBuilder( 'TribeEventsTicketObject' )->disableOriginalConstructor()
-			                          ->getMock();
-			$this->mock_ticket_meta = $this->getMockBuilder( 'TribeEventsTickets_TicketMeta' )
-			                               ->disableOriginalConstructor()->getMock();
-			$this->sut = new TribeEventsTicketStockObject();
 		}
 
 		protected function tearDown() {
@@ -24,7 +19,10 @@
 		 */
 		public function it_should_default_to_unlimited_stock() {
 
-			$this->assertInstanceOf( 'TribeEventsTickets_Stock_UnlimitedType', $this->sut->type );
+			list( $mock_ticket, $mock_ticket_meta ) = $this->get_mock_ticket_and_meta();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
+
+			$this->assertInstanceOf( 'TribeEventsTickets_Stock_UnlimitedType', $sut->type );
 		}
 
 		/**
@@ -32,8 +30,21 @@
 		 * it should default to unlimited local quantity
 		 */
 		public function it_should_default_to_unlimited_local_quantity() {
+			list( $mock_ticket, $mock_ticket_meta ) = $this->get_mock_ticket_and_meta();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
 
-			$this->assertEquals( TribeEventsTicketObject::UNLIMITED_STOCK, $this->sut->get_local_qty() );
+			$this->assertEquals( TribeEventsTicketObject::UNLIMITED_STOCK, $sut->get_local_qty() );
+		}
+
+		/**
+		 * @return array
+		 */
+		protected function get_mock_ticket_and_meta() {
+			$mock_ticket = $this->getMockBuilder( 'TribeEventsTicketObject' )->disableOriginalConstructor()->getMock();
+			$mock_ticket_meta = $this->getMockBuilder( 'TribeEventsTicket_TicketMeta' )->disableOriginalConstructor()
+			                         ->getMock();
+
+			return array( $mock_ticket, $mock_ticket_meta );
 		}
 
 		/**
@@ -41,9 +52,11 @@
 		 * it should default to not using the global stock
 		 */
 		public function it_should_default_to_not_using_the_global_stock() {
+			list( $mock_ticket, $mock_ticket_meta ) = $this->get_mock_ticket_and_meta();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
 
-			$this->assertFalse( $this->sut->type->is_global() );
-			$this->assertFalse( $this->sut->type->is_global_and_local() );
+			$this->assertFalse( $sut->type->is_global() );
+			$this->assertFalse( $sut->type->is_global_and_local() );
 		}
 
 		/**
@@ -51,8 +64,10 @@
 		 * it should default to emtpy string global stock id
 		 */
 		public function it_should_default_to_emtpy_string_global_stock_id() {
+			list( $mock_ticket, $mock_ticket_meta ) = $this->get_mock_ticket_and_meta();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
 
-			$this->assertEquals( '', $this->sut->get_global_stock_id() );
+			$this->assertEquals( '', $sut->get_global_stock_id() );
 		}
 
 		public function types_and_queries() {
@@ -70,12 +85,14 @@
 		 * @dataProvider types_and_queries
 		 */
 		public function it_should_allow_querying_for_the_stock_type( $type, $is_local, $is_global, $is_glocal, $is_unlimited ) {
-			$this->sut->type = $type;
+			list( $mock_ticket, $mock_ticket_meta ) = $this->get_mock_ticket_and_meta();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
+			$sut->type = $type;
 
-			$this->assertEquals( $is_local, $this->sut->type->is_local() );
-			$this->assertEquals( $is_global, $this->sut->type->is_global() );
-			$this->assertEquals( $is_glocal, $this->sut->type->is_global_and_local() );
-			$this->assertEquals( $is_unlimited, $this->sut->type->is_unlimited() );
+			$this->assertEquals( $is_local, $sut->type->is_local() );
+			$this->assertEquals( $is_global, $sut->type->is_global() );
+			$this->assertEquals( $is_glocal, $sut->type->is_global_and_local() );
+			$this->assertEquals( $is_unlimited, $sut->type->is_unlimited() );
 		}
 
 		/**
@@ -83,12 +100,14 @@
 		 * it should change to local stock type and set the quantity when setting a stock
 		 */
 		public function it_should_change_to_local_stock_type_and_set_the_quantity_when_setting_a_stock() {
+			list( $mock_ticket, $mock_ticket_meta ) = $this->get_mock_ticket_and_meta();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
 
-			$this->sut->set_stock( 23 );
+			$sut->set_stock( 23 );
 
-			$this->assertTrue( $this->sut->type->is_local() );
-			$this->assertEquals( 23, $this->sut->get_stock() );
-			$this->assertEquals( 23, $this->sut->get_local_qty() );
+			$this->assertTrue( $sut->type->is_local() );
+			$this->assertEquals( 23, $sut->get_stock() );
+			$this->assertEquals( 23, $sut->get_local_qty() );
 		}
 
 		/**
@@ -96,20 +115,26 @@
 		 * it should set stock meta when set_stock_meta method is called
 		 */
 		public function it_should_set_stock_meta_when_set_stock_meta_method_is_called() {
+			$mock_ticket = $this->getMockBuilder( 'TribeEventsTicketObject' )->disableOriginalConstructor()->getMock();
+			$mock_ticket_meta = $this->getMockBuilder( 'TribeEventsTicket_TicketMeta' )
+			                         ->setMethods( array( 'get_event_stock_meta' ) )->disableOriginalConstructor()
+			                         ->getMock();
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
+
 			$meta = array(
 				'use_global'      => false,
 				'use_local'       => true,
 				'local_qty'       => 23,
 				'global_stock_id' => 'default'
 			);
-			$this->sut->set_stock_meta( $meta );
+			$sut->set_stock_meta( $meta );
 
-			$this->assertTrue( $this->sut->type->is_local() );
-			$this->assertFalse( $this->sut->type->is_global_and_local() );
-			$this->assertFalse( $this->sut->type->is_global() );
-			$this->assertFalse( $this->sut->type->is_unlimited() );
-			$this->assertEquals( 23, $this->sut->get_local_qty() );
-			$this->assertEquals( 23, $this->sut->get_stock() );
+			$this->assertTrue( $sut->type->is_local() );
+			$this->assertFalse( $sut->type->is_global_and_local() );
+			$this->assertFalse( $sut->type->is_global() );
+			$this->assertFalse( $sut->type->is_unlimited() );
+			$this->assertEquals( 23, $sut->get_local_qty() );
+			$this->assertEquals( 23, $sut->get_stock() );
 		}
 
 		/**
@@ -117,8 +142,15 @@
 		 * it should return the min between local and global set if both set
 		 */
 		public function it_should_return_the_min_between_local_and_global_set_if_both_set() {
+			$mock_ticket = $this->getMockBuilder( 'TribeEventsTicketObject' )->disableOriginalConstructor()->getMock();
+			$mock_ticket_meta = $this->getMockBuilder( 'TribeEventsTicket_TicketMeta' )
+			                         ->setMethods( array( 'get_event_stock_meta' ) )->disableOriginalConstructor()
+			                         ->getMock();
+
+			$sut = new TribeEventsTicketStockObject( $mock_ticket, $mock_ticket_meta );
+
 			$event_stock_meta = array( 'default' => 12 );
-			$this->mock_ticket_meta->expects( $this->any() )->method( 'get_event_stock_meta' )
+			$mock_ticket_meta->expects( $this->any() )->method( 'get_event_stock_meta' )
 			                       ->will( $this->returnValue( $event_stock_meta ) );
 			$meta = array(
 				'use_global'      => true,
@@ -126,10 +158,10 @@
 				'local_qty'       => 23,
 				'global_stock_id' => 'default'
 			);
-			$this->sut->set_stock_meta( $meta );
+			$sut->set_stock_meta( $meta );
 
-			$this->assertEquals( 23, $this->sut->get_local_qty() );
-			$this->assertEquals( 12, $this->sut->get_global_qty() );
-			$this->assertEquals( 12, $this->sut->get_stock() );
+			$this->assertEquals( 23, $sut->get_local_qty() );
+			$this->assertEquals( 12, $sut->get_global_qty() );
+			$this->assertEquals( 12, $sut->get_stock() );
 		}
 	}
